9354c226a1bbc4ac86d0b2859cd2c6312dad5942
Matthias Wolfinger <m.wolfinger1@oth-aw.de>
2023-06-04 22:27:53 +0200

    created first function "convertColumn"

---
+++ mechanics/mechanics.js
@@ -0,0 +1,19 @@
+function convertColumn(column) {
+    if(column == "A" || column == "a") {
+        return 0;
+    } else if(column == "B" || column == "b") {
+        return 1;
+    } else if(column == "C" || column == "c") {
+        return 2;
+    } else if(column == "D" || column == "d") {
+        return 3;
+    } else if(column == "E" || column == "e") {
+        return 4;
+    } else if(column == "F" || column == "f") {
+        return 5;
+    } else if(column == "G" || column == "g") {
+        return 6;
+    } else if(column == "H" || column == "h") {
+        return 7;
+    }
+}
