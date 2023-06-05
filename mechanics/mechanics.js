0e146046b5497559bd7ce0734036b1d12e9cd3a6
Matthias Wolfinger <m.wolfinger1@oth-aw.de>
2023-06-05 13:00:22 +0200

    updated function move, added the King to checkMove

--- mechanics/mechanics.js
+++ mechanics/mechanics.js
@@ -1,3 +1,6 @@
+let schwarzerKoenig = [-1, -1];
+let weisserKoenig = [-1, -1];
+
 function convertColumn(column) {
     if(column == "A" || column == "a") {
         return 0;
@@ -35,16 +38,37 @@ function generateChess() {
     return schachbrett;
 }
 
+// COMING SOON
+// Funktion zum Überprüfen der Bedrohung! (englisch "being under check")
+function checkCheck(board, X, Y) {
+    // COMING SOON
+    return true;
+}
+
+// COMING SOON
+// Funktion zum Überprüfen, ob ein bedrohter König die Bedrohung durch einen Halbzug abwehren kann (wenn nein, ist er matt gesetzt und das Spiel ist vorbei!)
+function checkCheckmate(board, X, Y) {
+    // COMING SOON
+    return false;
+}
+
+// Rochade NOCH NICHT IMPLEMENTIERT!!!
 function checkMove(board, pieceX, pieceY, newX, newY) {
     // Fallunterscheidung je nach Typ der Figur
     if(pieceX < 0 || pieceX > 7 || pieceY < 0 || pieceY > 7 || newX < 0 || newX > 7 || newY < 0 || newY > 7) {
-        // ungültige Koordinaten
+        // ungueltige Koordinaten
         return false;
     } else if(board[pieceY][pieceX] == "-") {
         // keine Figur auf dem Ursprungsfeld!
         return false;
     } else if(board[pieceY][pieceX][1] == "Koenig") {
-        // COMING SOON
+        if((newX == pieceX + 1 && newY == pieceY) || (newX == pieceX - 1 && newY == pieceY) || (newX == pieceX && newY == pieceY + 1) || (newX == pieceX && newY == pieceY - 1) || (newX == pieceX + 1 && newY == pieceY + 1) || (newX == pieceX + 1 && newY == pieceY - 1) || (newX == pieceX - 1 && newY == pieceY + 1) || (newX == pieceX - 1 && newY == pieceY - 1)) {
+            // Zielfeld gueltig - liegt im Bewegungsbereich
+            if(checkCheck(board, newX, newY)) {
+                // Koenig geht auf kein bedrohtes Feld - dies darf er naemlich nicht!
+                return true;
+            }
+        }
     } else if(board[pieceY][pieceX][1] == "Dame") {
         // COMING SOON
     } else if(board[pieceY][pieceX][1] == "Turm") {
@@ -69,6 +93,22 @@ function move(board, pieceX, pieceY, newX, newY) {
     if(checkMove(board, pieceX, pieceY, newX, newY)) {
         board[newY][newX] = board[pieceY][pieceX];
         board[pieceY][pieceX] = "-";
+        if(board[newY][newX][1] == "Koenig") {
+            if(board[newY][newX][2] == "S") {
+                schwarzerKoenig[0] = newX;
+                schwarzerKoenig[1] = newY;
+            } else if(board[newY][newX][2] == "W") {
+                weisserKoenig[0] = newX;
+                weisserKoenig[1] = newY;
+            }
+        }
+
+        // Vielleicht wird das hier unten auch später in eine andere Funktion verschoben!!
+        if(checkCheckmate(board, schwarzerKoenig[0], schwarzerKoenig[1])) {
+            // weiss hat gewonnen
+        } else if(checkCheckmate(board, weisserKoenig[0], weisserKoenig[1])) {
+            // schwarz hat gewonnen
+        }
         return board;
     } else {
         return board;
