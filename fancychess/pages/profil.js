06125b8f3b6dc4c7e932fb5a5f8db7de3411c18c
Alex Mueller <a.mueller3@oth-aw.de>
2023-06-05 17:54:40 +0200

    Merge Frontend Branch in meinen

--- fancychess/pages/profil.js
+++ fancychess/pages/profil.js
@@ -1,23 +1,27 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/profil.module.css';
+import Link from "next/link";
 
 export default function Profil() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Profil</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="../public/logo.ico" />
       </Head>
       <body>
           <div className={styles.leftpannel}>
               <div className={styles.navpannel}>
-
+                  <h1>Fancy Chess</h1>
               </div>
           </div>
 
           <div className={styles.midpannel}>
               <div className={styles.profilpicture}></div>
               <div className={styles.profilpannel}>
+
+              </div>
+              <div className={styles.gamehistory}>
 
               </div>
           </div>
@@ -31,7 +35,7 @@ export default function Profil() {
         <style global jsx>{`
         html,
         body{
-          height: 100%;
+          height: 100vh;
           width: 100%;
           margin: 0;
           }
