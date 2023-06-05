39885c7fdd90f1cdb1398a0af822a6dd43de1a27
Matthias Wolfinger <m.wolfinger1@oth-aw.de>
2023-06-05 13:40:12 +0200

    Added the Queen to the function "checkMove"

--- mechanics/mechanics.js
+++ mechanics/mechanics.js
@@ -42,7 +42,7 @@ function generateChess() {
 // Funktion zum Überprüfen der Bedrohung! (englisch "being under check")
 function checkCheck(board, X, Y) {
     // COMING SOON
-    return true;
+    return false;
 }
 
 // COMING SOON
@@ -55,7 +55,10 @@ function checkCheckmate(board, X, Y) {
 // Rochade NOCH NICHT IMPLEMENTIERT!!!
 function checkMove(board, pieceX, pieceY, newX, newY) {
     // Fallunterscheidung je nach Typ der Figur
-    if(pieceX < 0 || pieceX > 7 || pieceY < 0 || pieceY > 7 || newX < 0 || newX > 7 || newY < 0 || newY > 7) {
+    if(pieceX == newX && pieceY == newY) {
+        // keine Bewegung, somit ungueltig, da Zugpflicht herrscht!
+        return false;
+    } else if(pieceX < 0 || pieceX > 7 || pieceY < 0 || pieceY > 7 || newX < 0 || newX > 7 || newY < 0 || newY > 7) {
         // ungueltige Koordinaten
         return false;
     } else if(board[pieceY][pieceX] == "-") {
@@ -64,13 +67,91 @@ function checkMove(board, pieceX, pieceY, newX, newY) {
     } else if(board[pieceY][pieceX][1] == "Koenig") {
         if((newX == pieceX + 1 && newY == pieceY) || (newX == pieceX - 1 && newY == pieceY) || (newX == pieceX && newY == pieceY + 1) || (newX == pieceX && newY == pieceY - 1) || (newX == pieceX + 1 && newY == pieceY + 1) || (newX == pieceX + 1 && newY == pieceY - 1) || (newX == pieceX - 1 && newY == pieceY + 1) || (newX == pieceX - 1 && newY == pieceY - 1)) {
             // Zielfeld gueltig - liegt im Bewegungsbereich
-            if(checkCheck(board, newX, newY)) {
+            if(!checkCheck(board, newX, newY)) {
                 // Koenig geht auf kein bedrohtes Feld - dies darf er naemlich nicht!
                 return true;
+            } else {
+                return false;
             }
+        } else {
+            return false;
         }
     } else if(board[pieceY][pieceX][1] == "Dame") {
-        // COMING SOON
+        if(pieceX == newX) {
+            if(pieceY > newY) {
+                let tmpy = pieceY;
+                pieceY = newY;
+                newY = tmpy;
+                // jetzt ist pieceY kleiner
+            }
+            // diese for-Schleife beachtet nur die Felder dazwischen, nicht Quell- und Zielfeld selber!
+            for(let y = pieceY + 1; y < newY; y++) {
+                if(board[y][pieceX] != "-") {
+                    // wenn irgendwo eine andere Figur ist, ist der Zug nicht zulaessig
+                    return false;
+                }
+            }
+            return true;
+        } else if(pieceY == newY) {
+            if(pieceX > newX) {
+                let tmpx = pieceX;
+                pieceX = newX;
+                newX = tmpx;
+                // jetzt ist pieceX kleiner
+            }
+            // diese for-Schleife beachtet nur die Felder dazwischen, nicht Quell- und Zielfeld selber!
+            for(let x = pieceX + 1; x < newX; x++) {
+                if(board[pieceY][x] != "-") {
+                    // wenn irgendwo eine andere Figur ist, ist der Zug nicht zulaessig
+                    return false;
+                }
+            }
+            return true;
+        } else if(pieceX - newX == pieceY - newY) {
+            // If fuer 1. und 3. Quadranten
+            if(pieceX > newX) {
+                let tmpx = pieceX;
+                let tmpy = pieceY;
+                pieceX = newX;
+                newX = tmpx;
+                pieceY = newY;
+                newY = tmpy;
+                // jetzt sind pieceX und pieceY kleiner
+            }
+            // diese for-Schleife beachtet nur die Felder dazwischen, nicht Quell- und Zielfeld selber!
+            for(let p = pieceX + 1; p < newX; p++) {
+                if(board[p][p] != "-") {
+                    // wenn irgendwo eine andere Figur ist, ist der Zug nicht zulaessig
+                    return false;
+                }
+            }
+            return true;
+        } else if(pieceX - newX == pieceY + newY) {
+            // If fuer 2. und 4. Quadranten
+            if(pieceX > newX) {
+                let tmpx = pieceX;
+                pieceX = newX;
+                newX = tmpx;
+                // jetzt ist pieceX kleiner
+            }
+            if(pieceY > newY) {
+                let tmpy = pieceY;
+                pieceY = newY;
+                newY = tmpy;
+                // jetzt ist pieceY kleiner
+            }
+            // diese for-Schleife beachtet nur die Felder dazwischen, nicht Quell- und Zielfeld selber!
+            for(let p = pieceX + 1; p < newX; p++) {
+                if(board[newY][p] != "-") {
+                    // wenn irgendwo eine andere Figur ist, ist der Zug nicht zulaessig
+                    return false;
+                }
+                newY--;
+            }
+            return true;
+        } else {
+            return false;
+        }
     } else if(board[pieceY][pieceX][1] == "Turm") {
         // COMING SOON
     } else if(board[pieceY][pieceX][1] == "Laeufer") {
