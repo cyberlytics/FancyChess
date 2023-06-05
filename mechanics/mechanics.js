02e4740ead96ff33121d9a432eb6c9709e5ae315
Matthias Wolfinger <m.wolfinger1@oth-aw.de>
2023-06-05 12:31:02 +0200

    added func. move, started dev. of func. checkMove

--- mechanics/mechanics.js
+++ mechanics/mechanics.js
@@ -18,7 +18,12 @@ function convertColumn(column) {
     }
 }
 
+// wie eine Figur aufgebaut sein wird:
+// Array
+// [ID, Typ, Farbe]
+
 function generateChess() {
+    // Funktion wird vermutlich noch umgeschrieben, um direkt ein Schachbrett mit Grundstellung zu generieren
     let schachbrett = [];
     for(let i = 0; i < 8; i++) {
         let unterbrett = [];
@@ -28,4 +33,44 @@ function generateChess() {
         schachbrett.push(unterbrett);
     }
     return schachbrett;
+}
+
+function checkMove(board, pieceX, pieceY, newX, newY) {
+    // Fallunterscheidung je nach Typ der Figur
+    if(pieceX < 0 || pieceX > 7 || pieceY < 0 || pieceY > 7 || newX < 0 || newX > 7 || newY < 0 || newY > 7) {
+        // ungültige Koordinaten
+        return false;
+    } else if(board[pieceY][pieceX] == "-") {
+        // keine Figur auf dem Ursprungsfeld!
+        return false;
+    } else if(board[pieceY][pieceX][1] == "Koenig") {
+        // COMING SOON
+    } else if(board[pieceY][pieceX][1] == "Dame") {
+        // COMING SOON
+    } else if(board[pieceY][pieceX][1] == "Turm") {
+        // COMING SOON
+    } else if(board[pieceY][pieceX][1] == "Laeufer") {
+        // COMING SOON
+    } else if(board[pieceY][pieceX][1] == "Springer") {
+        // COMING SOON
+    } else if(board[pieceY][pieceX][1] == "Bauer") {
+        // COMING SOON
+    } else {
+        alert("Programmierfehler in der checkMove-Funktion!");
+        return false;
+    }
+}
+
+function move(board, pieceX, pieceY, newX, newY) {
+    // Vorher CHECK mit Funktion checkMove, ob Zug gültig ist!
+    // wenn erfolgreich durchlaufen, dann bewegen
+    // Unterer Code sollte funktionieren.
+
+    if(checkMove(board, pieceX, pieceY, newX, newY)) {
+        board[newY][newX] = board[pieceY][pieceX];
+        board[pieceY][pieceX] = "-";
+        return board;
+    } else {
+        return board;
+    }
 }
