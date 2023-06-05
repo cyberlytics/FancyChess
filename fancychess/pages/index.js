0ade1d797f3fe8290ccc8fe45cf4bb47e30083f7
Alex Mueller <a.mueller3@oth-aw.de>
2023-06-05 17:52:20 +0200

    Merge von allen Frontend Branches

--- fancychess/pages/index.js
+++ fancychess/pages/index.js
@@ -1,41 +1,75 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import ChessBoard from './chess-board.js'
+import Link from 'next/link';
 
-export default function Profil() {
+export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Profil</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>FancyChess</title>
+          <link rel="icon" href="../public/logo.ico" />
       </Head>
       <body>
-          <div className={styles.leftpannel}>
-              <div className={styles.navpannel}>
+          <div className="section" id={styles.menu}>
+            <div is="innermenu">
+              <h1>Fancy Chess</h1>
 
-              </div>
+              <Link href="./profil" className={styles.link}>
+                <h2>Account</h2>
+              </Link>
+
+              <a href="./settings" className={styles.link}>
+                <h2>Settings</h2>
+              </a>
+
+              <a href="#" className={styles.link} id="logout">
+                <h2>Logout</h2>
+              </a>
+
+            </div>
+
           </div>
 
-          <div className={styles.midpannel}>
-              <div className={styles.profilpicture}></div>
-              <div className={styles.profilpannel}>
+          <div className="section" id={styles.game}>
+            <div classname="board" id={styles.board}>
+                <ChessBoard />
 
-              </div>
+            </div>
+
           </div>
 
-          <div className={styles.rightpannel}></div>
+          <div className="section" id={styles.log}>
 
+            <p id="time">00:00
+            </p>
+            
+            <button id="inviteLink">
+              Invite Link
+            </button>
+            
+            <button>
+              Start/End
+            </button>
+
+            <div id={styles.playerMoveHistory}>
+              <p>ertser Zug</p>
+              <p>zweiter Zug</p>
+            </div>
+
+          </div>
       </body>
 
-      <footer>
-      </footer>
-        <style global jsx>{`
+      <style global jsx>{`
         html,
         body{
-          height: 100%;
-          width: 100%;
+          background-image: url("../public/background.jpg");
+          height: 100vh;
           margin: 0;
           }
         `}</style>
+    
+
     </div>
   )
 }
