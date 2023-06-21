91785b3ba0789eb6db0bebd1d14c0c382e23353e
Alex Mueller <a.mueller3@oth-aw.de>
2023-06-21 13:31:41 +0200

    Pop up windows changes

--- fancychess/pages/win-lose-pop-up.js
+++ fancychess/pages/win-lose-pop-up.js
@@ -5,19 +5,32 @@ import styles from '../styles/win-lose-pop-up.module.css';
 export default function WinLosePopUp(props) {
 
     const win_pop_up = () => {
-        
+        return (
+            <h1>WINNER</h1>
+        );
     }
 
     const lose_pop_up = () => {
-    
+        return (
+            <div>
+                <h1>LOSE</h1>
+                <style>{`
+                .popup{
+                    background-color: rgb(255, 130, 130);
+                }
+                `}</style>
+            </div>
+        )
     }
 
 
     return (props.trigger) ? (
         <div className={styles.popup}>
             <div className={styles.popup_inner}>
-                <h1>HELLO</h1>
-                <button onClick={() => props.setTrigger(false)}>Back</button>
+                {lose_pop_up()}
+                <div className= {styles.profilpic_winner}></div>
+                <div className= {styles.profilpic_loser}></div>
+                <button onClick={() => props.setTrigger(false)} className={styles.accept_button} >Accept</button>
             </div>
         </div>
     ) : "";
