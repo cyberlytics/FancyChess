192d8cb1ad514fb21cb6a856261ceb326ffe9161
Alex Mueller <a.mueller3@oth-aw.de>
2023-06-22 14:35:18 +0200

    Profil changes

--- fancychess/pages/profil.js
+++ fancychess/pages/profil.js
@@ -1,8 +1,42 @@
 import Head from 'next/head';
 import styles from '../styles/profil.module.css';
 import Link from "next/link";
+import {Dropdown} from "@nextui-org/react";
 
 export default function Profil() {
+  const dropdownmenu = () => {
+      return(
+        <Dropdown>
+          <Dropdown.Button edit>edit</Dropdown.Button>
+          <Dropdown.Menu aria-label="Static Actions">
+            <Dropdown.Item key="W_Bauer">W_Bauer</Dropdown.Item>
+            <Dropdown.Item key="W_Turm">W_Turm</Dropdown.Item>
+            <Dropdown.Item key="W_Springer">W_Springer</Dropdown.Item>
+            <Dropdown.Item key="W_Laufer">W_Läufer</Dropdown.Item>
+            <Dropdown.Item key="W_Koenig">W_König</Dropdown.Item>
+            <Dropdown.Item key="W_Dame">W_Dame</Dropdown.Item>
+            <Dropdown.Item key="S_Bauer">S_Bauer</Dropdown.Item>
+            <Dropdown.Item key="S_Turm">S_Turm</Dropdown.Item>
+            <Dropdown.Item key="S_Springer">S_Springer</Dropdown.Item>
+            <Dropdown.Item key="S_Laufer">S_Läufer</Dropdown.Item>
+            <Dropdown.Item key="S_Koenig">S_König</Dropdown.Item>
+            <Dropdown.Item key="S_Dame">S_Dame</Dropdown.Item>
+          </Dropdown.Menu>
+          {dropdownchoice(Dropdown.key)}
+        </Dropdown>
+      )
+  }
+
+  const dropdownchoice = (key) => {
+    switch(key){
+      case "W_Bauer":{
+        return(
+          <image src="../public/Pawn-W.svg"/>
+        )
+      }
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,9 +45,10 @@ export default function Profil() {
       </Head>
       <body>
           <div className={styles.leftpannel}>
-              <div className={styles.navpannel}>
+            <Menu />
+              {/*<div className={styles.navpannel}>
                   <h1>Fancy Chess</h1>
-              </div>
+                </div>*/}
           </div>
 
           <div className={styles.midpannel}>
@@ -38,9 +73,13 @@ export default function Profil() {
           height: 100vh;
           width: 100%;
           margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
           }
         `}
         </style>
     </div>
   )
+
 }
